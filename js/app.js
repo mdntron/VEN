2ec8578c089c5537ab481dc21e647818cbce6499
taoge0303 <99592358+taoge0303@users.noreply.github.com>
2022-02-16 12:18:50 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -72,7 +72,16 @@ async function init() {
         // Mikko's TESTNET api key
         key: "pk_test_391E26A3B43A3350"
       }
-    }
+    },
+	walletlink: {
+	        package: WalletLink,
+	        options: {
+	          appName: "VEN",
+	           infuraId: "8043bb2cf99347b1bfadfb233c5325c0",
+			   appLogoUrl:"",
+			    darkMode: false,
+	        }
+	      },
   };
 
   web3Modal = new Web3Modal({
